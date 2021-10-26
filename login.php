4aff16853437164a4ee766d06387e4bc0ac75dbe
Ian A. Matias Rodriguez <90672536+Ian-Matias@users.noreply.github.com>
2021-10-26 16:48:27 -0400

    phpmyadmin databse connection(login.php,index.php)

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php 
+
+$loginCon = mysqli_connect('localhost','root','', 'login');
+
+if(isset($_POST['Username'])){
+    
+    $uname=$_POST['Username'];
+    $pass=$_POST['Password'];
+    
+    $sql="SELECT * FROM users WHERE username='".$uname."'AND pass='".$pass."' limit 1";
+    
+    $result=mysqli_query($loginCon,$sql);
+    
+    if(mysqli_num_rows($result)==1){
+        echo " You Have Successfully Logged in";
+        header('Location: ./pages/homepage.html');
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    }
+        
+}
+?>
